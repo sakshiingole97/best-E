c6c9a68b2d4cd84175cc4617f3b87beac29557b9
sakshiingole97 <85789479+sakshiingole97@users.noreply.github.com>
2021-06-13 13:10:37 +0530

    Add files via upload

---
+++ DAY2 CHALLENGE.py
@@ -0,0 +1,47 @@
+Python 3.6.6rc1 (v3.6.6rc1:1015e38be4, Jun 12 2018, 08:38:06) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> Hours="thirty"
+>>> print("Hours")
+Hours
+>>> Day="Thirty day"
+>>> print(Day[0])
+T
+>>> print(Day[3])
+r
+>>> challenge="I WILL WIN"
+>>> print(len(challenge))
+10
+>>> print(challenge.lower())
+i will win
+>>> a="30 days"
+>>> b="30 hours"
+>>> c=a+b
+>>> print(c)
+30 days30 hours
+>>> a="30 days"
+>>> b="30 hours"
+>>> c = a + " " + b
+>>> print(c)
+30 days 30 hours
+>>> text="Thirty days and Thirty hours"
+>>> x=text.casefold()
+>>> print(x)
+thirty days and thirty hours
+>>> text="thirty days and thirty hours"
+>>> y=text.capitalize()
+>>> print(y)
+Thirty days and thirty hours
+>>> text="thirty day thirty hours"
+>>> x=text.isalpha()
+>>> print(g)
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    print(g)
+NameError: name 'g' is not defined
+>>> print(x)
+False
+>>> text="thirty day thirty hours"
+>>> x=text.isalnum()
+>>> print(x)
+False
+>>> 
