98a3457a8f321476e0aac095da218d4f033a2854
sakshiingole97 <85789479+sakshiingole97@users.noreply.github.com>
2021-06-27 19:52:05 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,10 @@
+#3.	Import a module that picks random number and write a program to fetch a random number from 1 to  100 on every run
+import random
+r=random.randint(1,100)
+print(r)
+
+print()
+#4.	Import sys package and find the python path
+import sys
+s=sys.path
+print(s)
