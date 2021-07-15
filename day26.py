b1e357c0392ae3daac7924cae095d1fa2b23fc03
sakshiingole97 <85789479+sakshiingole97@users.noreply.github.com>
2021-07-15 10:56:37 +0530

    Add files via upload

---
+++ day26.py
@@ -0,0 +1,10 @@
+#1. Enumerate a python list and try to print the counter with the list value
+list1= ['Aditi','Ashwin','Rohan','Hardik','Saloni','vaishali']
+print(list(enumerate(list1)))
+
+print(enumerate(list1))
+
+
+#2. Enumerate a python tuple and try to print the counter with the tuple value
+tuple1=('Aditi','Ashwin','Rohan','Hardik','Saloni','vaishali')
+print(tuple(enumerate(tuple1)))
